6b8bdf7e4a73d3c3f248f7d09007772a9c9b3cf3
PG <plamen5020@gmail.com>
2023-08-13 17:32:40 +0300

    added some security preventions

--- MyTvShows/settings.py
+++ MyTvShows/settings.py
@@ -125,14 +125,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-#
-# STATICFILES_DIRS = (
-#     BASE_DIR / 'staticfiles/',
-# )
 
 STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
